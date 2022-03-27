372f905076ace47d2cfafe0b5acedc9270008ce2
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-27 08:27:34 +0100

    intro to performance

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log("Hey there");
+function addUpTo(n: number) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+function addUpTo2(n: number) {
+  return (n * (n + 1)) / 2;
+}
+
+console.time("addUpTo");
+console.log(addUpTo(1000000));
+console.timeEnd("addUpTo");
+console.time("addUpTo2");
+console.log(addUpTo2(1000000));
+console.timeEnd("addUpTo2");
