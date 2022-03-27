a4c373a225a1aa947d0aec2f0a952c05f512fc2a
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-27 09:01:42 +0100

    log at least and at most 5

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
-function printAllPairs(n: number) {
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      console.log(i, j);
-    }
+function logAtLeast5(n: number) {
+  for (let i = 1; i <= Math.max(n, 5); i++) {
+    console.log(i);
   }
 }
 
-printAllPairs(10); // O(n^2)
+logAtLeast5(1); // O(n)
+
+function logAtMost5(n: number) {
+  for (let i = 1; i <= Math.min(n, 5); i++) {
+    console.log(i);
+  }
+}
+
+logAtMost5(30); // O(1)
