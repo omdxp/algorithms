d4d99af8699a59e55eda2d9433f0b58575a834f7
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-27 09:51:26 +0100

    big o of arrays

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,9 @@
-let instructor = {
-  firstName: "Omar",
-  isInstructor: true,
-  favoriteNumbers: [1, 2, 3, 4],
-};
+let names = ["Omar", "Michael", "Melissa"];
+let values = [true, {}, [], 2, "awesome"];
 
-console.table(instructor);
-
-console.log(Object.keys(instructor)); // O(n)
-console.log(Object.values(instructor)); // O(n)
-console.log(Object.entries(instructor)); // O(n)
-console.log(instructor.hasOwnProperty("firstName")); // O(1)
+console.log(names[0]); // O(1) (access element)
+console.log(names.find((value) => value === "Omar")); // O(n) (search element)
+console.log(names.push("Raj")); // O(1) (add last element)
+console.log(names.unshift("Yasser")); // O(n) replace all next indices (add first element)
+console.log(names.pop()); // O(1) (remove last element)
+console.log(names.shift()); // O(n) replace all next indices (remove first element)
