d6a10975bf0132baa93dc759639a48c10ea317ef
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-29 14:23:36 +0100

    add undericted and directed edges to graph

--- src/app.ts
+++ src/app.ts
@@ -11,10 +11,37 @@ class Graph<T extends string | number | symbol> {
   addVertex(value: T) {
     if (!this._adjacencyList[value]) this._adjacencyList[value] = [];
   }
+
+  addUnderictedEdge(vertex1: T, vertex2: T) {
+    if (!this.checkVertices(vertex1, vertex2)) return;
+    this._adjacencyList[vertex1]?.push(vertex2);
+    this._adjacencyList[vertex2]?.push(vertex1);
+  }
+
+  addDirectedEdge(vertex1: T, vertex2: T) {
+    if (!this.checkVertices(vertex1, vertex2)) return;
+    this._adjacencyList[vertex1]?.push(vertex2);
+  }
+
+  private checkVertices(vertex1: T, vertex2: T): boolean {
+    let foundVertex1 = Object.prototype.hasOwnProperty.call(
+      this._adjacencyList,
+      vertex1
+    );
+    let foundVertex2 = Object.prototype.hasOwnProperty.call(
+      this._adjacencyList,
+      vertex2
+    );
+    return foundVertex1 && foundVertex2;
+  }
 }
 
 let g = new Graph();
 g.addVertex("Tokyo");
 g.addVertex("San Francisco");
+g.addVertex("Berlin");
+
+g.addUnderictedEdge("Tokyo", "San Francisco"); // Undirected graph
+g.addDirectedEdge("San Francisco", "Berlin");
 
 console.log(g.adjacencyList);
