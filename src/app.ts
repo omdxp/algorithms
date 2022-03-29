fddd76f4e74c544646960dcdf40704b3c235f1b5
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-29 14:54:06 +0100

    remove undirect and direct edge from graph

--- src/app.ts
+++ src/app.ts
@@ -12,15 +12,22 @@ class Graph<T extends string | number | symbol> {
     if (!this._adjacencyList[value]) this._adjacencyList[value] = [];
   }
 
-  addUnderictedEdge(vertex1: T, vertex2: T) {
+  addEdge(vertex1: T, vertex2: T, type: "direct" | "undirect") {
     if (!this.checkVertices(vertex1, vertex2)) return;
     this._adjacencyList[vertex1]?.push(vertex2);
-    this._adjacencyList[vertex2]?.push(vertex1);
+    type === "undirect" && this._adjacencyList[vertex2]?.push(vertex1);
   }
 
-  addDirectedEdge(vertex1: T, vertex2: T) {
-    if (!this.checkVertices(vertex1, vertex2)) return;
-    this._adjacencyList[vertex1]?.push(vertex2);
+  removeEdge(vertex1: T, vertex2: T, type: "direct" | "undirect") {
+    if (!this.checkEdge(vertex1, vertex2, type)) return;
+    this._adjacencyList[vertex1] = this._adjacencyList[vertex1]?.filter(
+      (v) => v !== vertex2
+    );
+    type === "undirect"
+      ? (this._adjacencyList[vertex2] = this._adjacencyList[vertex2]?.filter(
+          (v) => v !== vertex1
+        ))
+      : null;
   }
 
   private checkVertices(vertex1: T, vertex2: T): boolean {
@@ -34,6 +41,20 @@ class Graph<T extends string | number | symbol> {
     );
     return foundVertex1 && foundVertex2;
   }
+
+  private checkEdge(
+    vertex1: T,
+    vertex2: T,
+    type: "direct" | "undirect"
+  ): boolean {
+    if (!this.checkVertices(vertex1, vertex2)) return false;
+    let result =
+      type === "direct"
+        ? this._adjacencyList[vertex1]!.includes(vertex2)
+        : this._adjacencyList[vertex1]!.includes(vertex2) &&
+          this._adjacencyList[vertex2]!.includes(vertex1);
+    return result;
+  }
 }
 
 let g = new Graph();
@@ -41,7 +62,10 @@ g.addVertex("Tokyo");
 g.addVertex("San Francisco");
 g.addVertex("Berlin");
 
-g.addUnderictedEdge("Tokyo", "San Francisco"); // Undirected graph
-g.addDirectedEdge("San Francisco", "Berlin");
+g.addEdge("Tokyo", "San Francisco", "undirect"); // Undirected graph
+g.addEdge("San Francisco", "Berlin", "direct");
+
+g.removeEdge("Tokyo", "San Francisco", "undirect");
+g.removeEdge("San Francisco", "Berlin", "direct");
 
 console.log(g.adjacencyList);
