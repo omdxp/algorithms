9c9062aa676fb15e75d51e135cbbbbeccd85768e
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-29 10:15:50 +0100

    improve hash function

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
-function hash(key: string, length: number) {
+function hash(key: string, arrLength: number) {
   let total = 0;
-  for (let char of key) {
+  let WEIRD_PRIME = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
     let value = char.charCodeAt(0) - 96;
-    total = (total + value) % length;
+    total = (total * WEIRD_PRIME + value) % arrLength;
   }
   return total;
 }
 
-console.log(hash("pink", 10));
-console.log(hash("blue", 10));
-console.log(hash("red", 10));
+console.log(hash("pink", 13));
+console.log(hash("blue", 13));
+console.log(hash("red", 13));
