eefca2d5de7c4dd4138542cebac53a99a11b79e4
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-29 10:36:21 +0100

    hash table with separate chaining set

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,49 @@
-function hash(key: string, arrLength: number) {
-  let total = 0;
-  let WEIRD_PRIME = 31;
-  for (let i = 0; i < Math.min(key.length, 100); i++) {
-    let char = key[i];
-    let value = char.charCodeAt(0) - 96;
-    total = (total * WEIRD_PRIME + value) % arrLength;
+class HashTable<T> {
+  private _keyMap: Array<Array<[string, T]>>;
+  public get keyMap(): Array<Array<[string, T]>> {
+    return this._keyMap;
   }
-  return total;
+
+  constructor(public size: number = 53) {
+    this._keyMap = new Array(size);
+  }
+
+  private hash(key: string) {
+    let total = 0;
+    let WEIRED_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRED_PRIME + value) % this._keyMap.length;
+    }
+    return total;
+  }
+
+  set(key: string, value: T) {
+    let hashedKey = this.hash(key);
+    if (!this._keyMap[hashedKey]) this._keyMap[hashedKey] = [[key, value]];
+    else {
+      this._keyMap[hashedKey].push([key, value]);
+    }
+  }
+
+  get(key: string): Array<[string, T]> | null {
+    let hashedKey = this.hash(key);
+    if (!this._keyMap[hashedKey]) return null;
+    return this._keyMap[hashedKey];
+  }
 }
 
-console.log(hash("pink", 13));
-console.log(hash("blue", 13));
-console.log(hash("red", 13));
+let hashTable = new HashTable(10);
+
+hashTable.set("pink", 2);
+hashTable.set("blue", true);
+hashTable.set("salmon", "here");
+
+console.log(hashTable.get("pink"));
+console.log(hashTable.get("blue"));
+console.log(hashTable.get("salmon"));
+
+console.log("----");
+
+console.log(hashTable.keyMap);
