9e9c94ebb152f12d54620a1ff2afeec3dd45bccc
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-04-01 22:07:59 +0100

    pascal triangle

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,26 @@
-function radixSort(array: number[]) {
-  if (array.length < 2) return array;
-  let max = array[0];
-  for (let i = 1; i < array.length; i++) {
-    if (array[i] > max) {
-      max = array[i];
-    }
+let rows = 5;
+let count = 0;
+let count1 = 0;
+let k = 0;
+let str = "";
+for (let i = 1; i <= rows; ++i) {
+  for (let space = 1; space <= rows - i; ++space) {
+    str += "  ";
+    ++count;
   }
-  const iterationCount = max.toString().length;
-  for (let digit = 0; digit < iterationCount; digit++) {
-    const bucket: number[][] = Array.from({ length: 10 }, () => []);
-    for (let i = 0; i < array.length; i++) {
-      const digitValue = Math.floor(array[i] / Math.pow(10, digit)) % 10;
-      bucket[digitValue].push(array[i]);
+
+  while (k !== 2 * i - 1) {
+    if (count <= rows - 1) {
+      str += `${i + k} `;
+      ++count;
+    } else {
+      ++count1;
+      str += `${i + k - 2 * count1} `;
     }
-    array = [].concat(...bucket);
+    ++k;
   }
-  return array;
+  count1 = count = k = 0;
+  str += "\n";
 }
 
-console.log(radixSort([745, 34, 24, 36, 98]));
+console.log(str);
