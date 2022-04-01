dbd60bcdda9cf8c679da5dde98cced2243ed9fb7
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-04-01 21:26:55 +0100

    tabulation version for the fibonacci series

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,19 @@ function fib(n: number, memo: number[] = []): number {
   return res;
 }
 // O(2^n) well to be more precise O(1.6^n) for normal fibonacci function
-console.log(fib(100)); // O(n)
+console.time("memo");
+console.log(fib(1000)); // O(n); possible of stackoverflow (space complexity is worse)
+console.timeEnd("memo");
+
+// tabulation version
+function fibTab(n: number) {
+  if (n <= 2) return 1;
+  let fibNums = [0, 1, 1];
+  for (let i = 3; i <= n; i++) {
+    fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+  }
+  return fibNums[n];
+}
+console.time("table");
+console.log(fibTab(1000)); // O(n) but better for space complexity
+console.timeEnd("table");
