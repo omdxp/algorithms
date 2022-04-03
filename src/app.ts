9e59199b517e4ada4952201d914e75da4524b76c
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-04-03 11:37:28 +0100

    radix sort (second implementation)

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,32 @@
-function countingSort(array: number[]) {
-  let min = array[0];
-  if (array.some((value) => value < 0)) {
-    for (let i = 1; i < array.length; i++) if (array[i] < min) min = array[i];
-  }
-  if (min < 0) {
-    for (let i = 0; i < array.length; i++) array[i] += Math.abs(min);
-  }
-  let max = array[0];
-  for (let i = 1; i < array.length; i++) if (array[i] > max) max = array[i];
-  let countArray = Array.from({ length: max + 1 }, () => 0);
-  for (let i = 0; i < array.length; i++) {
-    countArray[array[i]]++;
-  }
-  for (let i = 1; i < countArray.length; i++) {
-    countArray[i] = countArray[i - 1] + countArray[i];
-  }
+// O(n + k) where n is the array length, k is 10 (0 - 9)
+function countSort(array: number[], pos: number) {
+  let countArr = Array.from({ length: 10 }, () => 0);
   let result: number[] = Array.from({ length: array.length });
+  for (let i = 0; i < array.length; i++) {
+    const digit = Math.floor((array[i] / pos) % 10);
+    countArr[digit]++;
+  }
+  for (let i = 1; i < countArr.length; i++) {
+    countArr[i] = countArr[i - 1] + countArr[i];
+  }
   for (let i = array.length - 1; i >= 0; i--) {
-    result[--countArray[array[i]]] = array[i];
+    const digit = Math.floor((array[i] / pos) % 10);
+    result[--countArr[digit]] = array[i];
   }
-  if (min < 0) {
-    for (let i = 0; i < result.length; i++) result[i] -= Math.abs(min);
+  for (let i = 0; i < result.length; i++) {
+    array[i] = result[i];
   }
-  return result;
 }
 
-const unsortedArray = [1, 5, 3, -9, 0, 11, 2, 2, 0, 4, 7, 3, 9, 0, 11, 2];
-const sortedArray = countingSort(unsortedArray);
-console.log(sortedArray);
+// O(d * (n + k)) where d is the number of digits for the max value
+function radixSort(array: number[]) {
+  let max = array[0];
+  for (let i = 0; i < array.length; i++) if (array[i] > max) max = array[i];
+  for (let pos = 1; Math.floor(max / pos) > 0; pos *= 10) {
+    countSort(array, pos);
+  }
+}
+
+const unsortedArray = [432, 8, 530, 90, 88, 231, 11, 45, 677, 199];
+radixSort(unsortedArray);
+console.log(unsortedArray);
